c2e4647ddeb45d0985f952e682e789584906f6c4
nadalperian <sibisantha@gmail.com>
2024-02-16 08:13:21 -0500

    Renamed folders - to keep in order

---
+++ 04Python_Variables/100_Variables.py
@@ -0,0 +1,15 @@
+# datatype of variable need not be declared
+# datatype means int or string in other languages like java
+# datatype is required in java before x or y like int or string
+
+x = 6 
+y = "Sibi"
+
+print(x)
+print(y)
+
+a = 30
+b = 40
+c = a+b
+
+print(c)
