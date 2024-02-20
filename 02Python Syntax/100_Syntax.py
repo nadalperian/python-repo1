960d87dc1921104f8fbc04d0d4b0a60dbef5afea
sibi <sibisantha2008@gmail.com>
2024-02-19 21:29:08 -0500

    Variable programs corrected

--- 02Python Syntax/100_Syntax.py
+++ 02Python Syntax/100_Syntax.py
@@ -1,6 +1,2 @@
-if 5 > 2:
- print("Five is greater than two!") 
-
-
-if 5 > 2:
-        print("Five is greater than two!") 
+if 7 > 3:
+    print("You are correct!")
--- 02Python Syntax/101_Variable.py
+++ 02Python Syntax/101_Variable.py
@@ -1,2 +1,15 @@
-print("sibi")
-print("santha")
+age = 15
+name1 = "santha"
+name2 = "santha"
+print(name1)
+
+person1 = "santosh"
+person2 = person1
+print(person2)
+
+height = "5"
+height2 = int(height)
+height3 = 2.5
+height4 = height2 + height3
+height5 = float(height2)
+print(height5)
